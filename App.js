4a2112701d13cdf9910c893566b4e89f79f91bbc
Janika Savela <janika_s@hotmail.com>
2023-10-17 14:57:08 +0300

    viesti

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Current weather</Text>
+      <Position />
     </View>
   );
 }
@@ -13,8 +14,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff'
   },
+  heading: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 8,
+    marginBottom: 8
+  },
+  input: {
+    backgroundColor: '#F0F0F0',
+    borderColor: '#FAFAFA',
+    height: 40,
+    margin: 8
+  },
+  list: {
+    margin: 8
+  },
+  row: {
+    height: 30
+  }
 });
---
+++ components/Position.js
@@ -0,0 +1,42 @@
+import * as Location from 'expo-location';
+import Weather from './Weather';
+import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+
+export default function Position () {
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    const [message, setMessage] = useState('Retrieving location...')
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+    (async () => { 
+        let {status} = await Location.requestForegroundPermissionsAsync()
+        console.log(status)
+       try {
+        if (status !== 'granted') {
+            setMessage('Location not permitted.')
+        } else {
+            const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
+            setLatitude(position.coords.latitude)
+            setLongitude(position.coords.longitude)
+            setMessage('Location retrieved')
+        }
+    }
+       catch (error) {
+        setMessage('Error retrieving location')
+        console.log(error)
+       }
+       setIsLoading(false)
+    }
+    )() }, [])
+
+    return (
+        <View>
+            <Text>{latitude.toFixed(3)},{longitude.toFixed(3)}</Text>
+            <Text>{message}</Text>
+            {isLoading === false && 
+            <Weather latitude={latitude} longitude={longitude}/>}
+        </View>
+    )
+}
